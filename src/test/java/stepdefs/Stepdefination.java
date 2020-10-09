b233ee3f7c7de9303becf4548ed8c7044242800b
amolneel <amolneel03@gmail.com>
2020-10-09 17:55:42 +0530

    make some changes in stepdefs class

--- src/test/java/stepdefs/Stepdefination.java
+++ src/test/java/stepdefs/Stepdefination.java
@@ -8,7 +8,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -37,17 +39,14 @@ public class Stepdefination {
 	@After
 	public void QuiteAll() {
 
-		TakesScreenshot scrnShot = (TakesScreenshot)driver;
-		byte[] data =scrnShot.getScreenshotAs(OutputType.BYTES);
-
-
-		//scn.attach(data, "image/png", "screenshot_01");
-
+		
+		
+		TakesScreenshot scrnShot = (TakesScreenshot) driver;
+		byte[] data = scrnShot.getScreenshotAs(OutputType.BYTES);		
 		driver.quit();
-		scn.log("User quit the browser!...");		
+		scn.log("User quit the browser!...");
 
 	}
-
 
 	@Given("User opened Browser")
 	public void user_opened_browser() {
@@ -65,15 +64,16 @@ public class Stepdefination {
 		String expected = "Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in";
 		String actual = driver.getTitle();
 		Assert.assertEquals("Page Title validdation", expected, actual);
-		scn.log("Page title validation successfull. Actual title: " + actual );
+		scn.log("Page title validation successfull. Actual title: " + actual);
 
 	}
 
 	@When("User Search for product {string}")
 	public void user_search_for_product(String ProductName) {
-		cmnpageobject = new CmnPageObject(driver);
+		cmnpageobject = new CmnPageObject(driver,scn);
 		cmnpageobject.SetSearchTextBox(ProductName);
 		cmnpageobject.ClickOnSearchBtn();
+		scn.log("user search product successfully!...");
 	}
 
 	@Then("Search Result page is displayed")
@@ -81,43 +81,81 @@ public class Stepdefination {
 		String expected = "Amazon.in : Redmi Note 9";
 		String actual = driver.getTitle();
 		Assert.assertEquals(expected, actual);
+		scn.log("search result page is displayed successfully!...");
 
 	}
 
 	@Then("product list should appear pertaining to the product search as {string}")
 	public void product_list_should_appear_pertaining_to_the_product_search_as(String ProductName) {
-		searchpageobjects = new SearchPageObject(driver);
+		searchpageobjects = new SearchPageObject(driver,scn);
 		searchpageobjects.ValidateProductList(ProductName);
+		scn.log("product list apeared successfully!...");
 	}
 
 	@When("User click on any product")
-	public void user_click_on_any_product() 
-	{
-		searchpageobjects = new SearchPageObject(driver);
+	public void user_click_on_any_product() {
+		searchpageobjects = new SearchPageObject(driver,scn);
 		searchpageobjects.UserClickOnAnyProduct();
+		scn.log("user clicked on product successfully!...");
 
 	}
+
 	@Then("Product Description is displayed in new tab")
-	public void product_description_is_displayed_in_new_tab() 
-	{
+	public void product_description_is_displayed_in_new_tab() {
 		String ExpectedPageTitle = "Amazon.in : Redmi note 9";
 		String ActualPageTitle = driver.getTitle();
-		System.out.println("the actual page title is"+ActualPageTitle);
+		System.out.println("the actual page title is" + ActualPageTitle);
 		Assert.assertTrue(ActualPageTitle.equalsIgnoreCase(ExpectedPageTitle));
+		scn.log("product descriprion is displayed successfully!...");
 
 	}
-
 
 	@Then("User is click on Add to cart")
-	public void user_is_click_on_add_to_cart() 
-	{
-		searchpageobjects = new SearchPageObject(driver);
+	public void user_is_click_on_add_to_cart() {
+		searchpageobjects = new SearchPageObject(driver,scn);
 		searchpageobjects.UserClickOnAddToCartBtn();
+		scn.log("user clicked on add to cart btn successfully!...");
 	}
 
+	@Then("User clicked on with exchange button")
+	public void user_clicked_on_with_exchange_button() {
+		searchpageobjects = new SearchPageObject(driver,scn);
+		searchpageobjects.UserClickedOnExchangeBtn();
+		scn.log("user clicked on exchange btn successfully!...");
 
+	}
 
+	@When("User clicked on choose phone to exchange")
+	public void user_clicked_on_choose_phone_to_exchange() {
+		searchpageobjects = new SearchPageObject(driver,scn);
+		searchpageobjects.UserClickedOnChoosePhoneToExchange();
+		scn.log("user clicked on phone to exchange btn successfully!...");
 
+	}
+
+	@When("User select brand of mobile and select model")
+	public void user_select_brand_of_mobile_and_select_model() {
+		searchpageobjects = new SearchPageObject(driver,scn);
+		searchpageobjects.UserSelectBrand_ModelOfMobile();
+		scn.log("user select brand and model successfully!...");
+
+	}
+
+	@Then("fixed exchange value will appear")
+	public void fixed_exchange_value_will_appear() {
+		searchpageobjects = new SearchPageObject(driver,scn);
+		searchpageobjects.exchangeValueWillAppear();
+		scn.log("exchange value will appeared successfully!...");
+
+	}
+	
+	@Then("User chang the specification and click on One year total damage protection")
+	public void user_chang_the_specification_and_click_on_one_year_total_damage_protection()
+	{
+		searchpageobjects = new SearchPageObject(driver,scn);
+		searchpageobjects.userChangeSpecificationandClickOnDamageProtection();
+		scn.log("user change specification and click on protection successfully!...");
+	}
 
 
 }
